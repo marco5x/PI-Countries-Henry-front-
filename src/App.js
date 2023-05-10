ad95704f351026dd31736d2825d15336dec94f2d
MarcosX <79601596+Marco5X@users.noreply.github.com>
2023-05-09 23:19:45 -0300

    Avances en el comopente dountryDtail

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import { LandingPage } from "./components/LandingPage/LandingPage";
-import { HomePage } from './components/HomePage/HomePage';
-import { FormActivity } from './components/FormActivity/FormActivity';
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+import { HomePage } from './pages/HomePage/HomePage';
+import { FormActivity } from './pages/FormActivity/FormActivity';
 import { CountryDetail } from './components/CountryDetail/CountryDetail';
 import { Layout } from './components/Layout/Layout';
 import { ErrorPage } from './components/Error/ErrorPage';
 
 
-function App() {
+
+function App() {    
     return (
         <Routes>
             <Route exact path='/' element={<LandingPage />} />
             <Route path='/countries' element={<Layout />}>
                 <Route index element={<HomePage />} />
-                <Route path=':id' render={({ match }) => <CountryDetail id={match.params.id} />} />
+                <Route path=':id' element = {<CountryDetail id="ARG" />} />
             </Route>
             <Route path='/activities' element={<FormActivity />} />
             
-            <Route path='*' element={<ErrorPage/>} />
-   
+            <Route path='*' element={<ErrorPage/>} />   
         </Routes>
     );
 }
--- src/components/CountryDetail/CountryDetail.jsx
+++ src/components/CountryDetail/CountryDetail.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import style from './CountryDetail.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-//import { countryDetail } from '../../Actions/index';
+import { Link, useParams } from 'react-router-dom';
+import { countryDetail } from '../../features/country/countrySlice';
 
 export const CountryDetail = ({ id }) => {
     const dispatch = useDispatch()
-    const country = useSelector(state => state.countryDetail)
+    const params = useParams()
+    const country = useSelector((state) => state.country)
+   
     const [state, setState] = useState(false)
 
     useEffect(() => {
-        // dispatch(countryDetail(id))
+        dispatch(countryDetail(id))
         setState(true)
     }, [])
+
+    const countrie = country
 
     return (
         <> {!state ? <div>Loading...</div> :
@@ -22,20 +26,21 @@ export const CountryDetail = ({ id }) => {
                         <Link className={style.buttonLink} to='/countries'> GO HOME </Link>
                     </div>
                     <div className={style.detailContainer} >
-                        <h1 key={country.id}>{country.name} ({country.id})</h1>
+                    {console.log(countrie)}
+                        <h1 key={countrie.id}>{countrie.name} ({countrie.id})</h1>
                         <figure >
-                            <img src={country.flag} alt='img not found 😔' />
+                            <img src={countrie.flag} alt='img not found 😔' />
                         </figure>
-                        <h2>Capital: {country.capital}</h2>
-                        <h2>Region: {country.region}</h2>
-                        <h2>Subregion: {country.subregion}</h2>
-                        <h2>Population: {country.population}</h2>
-                        <h2>Area: {country.area} km²</h2>
+                        <h2>Capital: {countrie.capital}</h2>
+                        <h2>Region: {countrie.region}</h2>
+                        <h2>Subregion: {countrie.subregion}</h2>
+                        <h2>Population: {countrie.population}</h2>
+                        <h2>Area: {countrie.area} km²</h2>
                     </div>
                     <div className={style.detailContainer}>
                         <h1>Dreams of tourist activities</h1>
                         <hr />
-                        {country.activities.length === 0 ? <p>There are no tourist dreams in this country</p> :
+                        {!country.activities ? <p>There are no tourist dreams in this country</p> :
                         <div> <ul>
                                 {country.activities.map((actTour) => (
                                     <li key={actTour.name}>
